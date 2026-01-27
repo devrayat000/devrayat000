087790b6abc5b8572a8d0ab443e01e67b9bcec60
Zul Ikram Musaddik Rayat <rayathossain49@gmail.com>
2026-01-27 15:26:56 +0600

    feat: configure base path and asset prefix in next.config.ts; add .nojekyll file

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  // Set the base path to your repository name, including a leading slash
+  basePath: "/devrayat000",
+  assetPrefix: "/devrayat000/", // Ensures assets like images/CSS load correctly
 };
 
 export default nextConfig;
